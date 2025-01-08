5501b4abfe92be0c6e2c16d0f6844b1f1adf9f3a
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-08 04:15:39 +0000

    continue config data

--- Basics_02/04.1_objectsInsideArrays.js
+++ Basics_02/04.1_objectsInsideArrays.js
@@ -1,1 +1,19 @@
-// 
+// there are some objects inside the objects:
+
+// when the data comes form the serves it can be in the below format like :
+
+const user = [
+    {
+        user : 1,
+        email : "dhaval@google.com"
+    }, 
+    {
+        user : 2,
+        email : "patel@microsoft.com"
+    },
+    {
+        user : 3,
+        email : "dp@meta.com"
+    }
+]
+console.log(user[1]);
