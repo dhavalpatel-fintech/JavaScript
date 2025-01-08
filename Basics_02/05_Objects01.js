1956d4fd5305a2c00cef09ee6b1d18519feda228
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-08 04:47:24 +0000

    continue config data

---
+++ Basics_02/05_Objects01.js
@@ -0,0 +1,15 @@
+//  Objects and the de-structuring of the data using the objects methods and the functions 
+
+const course = {
+    courseName : "Java Script form Chai aur Code",
+    cousreInstructor : "Hitesh Choudhary",
+    coursePrice : "999/-",
+}
+
+// methods to call out the key: value pairs 
+const{cousreInstructor:Instructor} = course
+console.log(Instructor);
+
+// what is an API??
+// Jab bhi aap apna kam kisi or ke sar pe dalte ho that is called an API.
+// Just like in a restaurant the menu card is the API menu and the product or an item you re getting is called the API.
