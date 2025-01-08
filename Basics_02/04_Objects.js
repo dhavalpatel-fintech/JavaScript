dff2239a3ce0c44ccc261ec058227a8469626cef
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-08 04:09:54 +0000

    continue config data

--- Basics_02/04_Objects.js
+++ Basics_02/04_Objects.js
@@ -27,4 +27,16 @@ console.log(user["full name"].fisrt.last);
 
 // ################################## the new things in the objects ####################################
 
+const obj1 = {1:"a", 2:"b"};
+const obj2 = {3:"c", 4:"d"};
+const obj3 = {5:"e", 6:"f"};
+
+// assigning the values in the object 3
+
+const obj4 = Object.assign({}, obj1, obj2, obj3);
+console.log(obj4);
+
+// the simple and the best way to do the concat is:
+const obj5 = {...obj1, ...obj2, ...obj3, ...obj4};
+console.log(obj5);
 
