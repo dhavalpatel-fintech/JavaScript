62b509e03eb1b54c65d4300ab21efcea36855589
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-09 04:52:56 +0000

    continue config data

--- Basics_03/04_IIFE.js
+++ Basics_03/04_IIFE.js
@@ -1,2 +1,33 @@
 // immediately invoked function expression (IIFE);
 
+
+// use of the IIFE function:
+
+//  1. to invoke immediately >> the private function
+
+//  2. to prevent the global scope's pollution we use the IIFE concept in the java script
+
+//  () ();
+
+
+// IIFE Function:
+
+// normal function
+(function cac() {
+    console.log("Chai aur Code");
+})();
+
+
+// arrow function
+
+( () =>{
+    console.log("Chai aur code Full Stack Web Development!!")
+})();
+
+// along with the argument and the parameters 
+
+( (name) =>{
+    console.log(`The name is: ${name}`);
+})("Dhaval Patel");
+
+
