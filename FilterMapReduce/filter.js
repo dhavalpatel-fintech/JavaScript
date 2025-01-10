26f06c4f2081603fcd43934a2d7ff064be58cd53
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-10 05:54:28 +0000

    continue config data

---
+++ FilterMapReduce/filter.js
@@ -0,0 +1,6 @@
+// filter in the java script is used with the fnction and you can get the > or < number fomr the array
+// node FilterMapReduce/filter.js
+const myNum = [1,2,3,4,5,6,7,8,9,10];
+
+const newNums = myNum.filter( (num)=> num>4);
+console.log(newNums);
