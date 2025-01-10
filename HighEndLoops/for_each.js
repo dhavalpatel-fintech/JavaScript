85fe0a8b015d32b0acd8c258399b43d7afa50bc6
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-10 05:16:34 +0000

    continue config data

--- HighEndLoops/for_each.js
+++ HighEndLoops/for_each.js
@@ -15,4 +15,37 @@ fsd.forEach( (item) =>{
 })
 
 
-// 
+// function to function call in teh java script using the for each
+
+function printme(item){
+    console.log(item);
+}
+coding.forEach(printme);
+
+
+// for each and the function with the index and the value in it:
+
+let sde = ["frontend", "backend", "dsa", "sd"]
+sde.forEach((item, index , arr)=>{
+    console.log(item, index, arr);
+});
+
+// object in the array
+const sde01 = [
+    {
+        languageName : "javascript",
+        languageFileName : "js",
+    },
+    {
+        languageName : "java",
+        languageFileName : "java",
+    },
+    {
+        languageName : "python",
+        languageFileName : "py",
+    },
+    {
+        languageName : "html",
+        languageFileName : "html",
+    },
+]
