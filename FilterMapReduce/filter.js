ed2b4e72f0689a47b957f04e2de9a648d05f50e1
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-10 06:09:09 +0000

    continue config data

--- FilterMapReduce/filter.js
+++ FilterMapReduce/filter.js
@@ -2,5 +2,16 @@
 // node FilterMapReduce/filter.js
 const myNum = [1,2,3,4,5,6,7,8,9,10];
 
-const newNums = myNum.filter( (num)=> num>4);
-console.log(newNums);
+// 1.
+// const newNums = myNum.filter( (num)=> num>4);
+// console.log(newNums);
+
+// 2. implicit function
+// console.log(myNum.filter( (num)=> num < 5 ));
+
+// 3. implicit functioj with the return values in it:
+console.log(myNum.filter( (num)=>{
+    return num>4;
+} ));
+
+// 4. 
