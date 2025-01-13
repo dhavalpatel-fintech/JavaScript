9088d2088c61eebc238c93274214ade69949361f
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-13 17:46:25 +0000

    project_06

---
+++ Project_06/project_06.js
@@ -0,0 +1,23 @@
+// getting the reference;
+const insert = document.getElementById('insert');
+
+// calling the whole window function:
+
+window.addEventListener('keydown', (e)=>{
+    insert.innerHTML = `
+    <div class='color'>
+    <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+    </table>
+    </div>
+    `
+});
