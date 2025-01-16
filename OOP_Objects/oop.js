865ba6be62bec7c69de89f288dccd0624087a884
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-16 03:46:04 +0000

    oops and objects

--- OOP_Objects/oop.js
+++ OOP_Objects/oop.js
@@ -21,6 +21,45 @@ console.log(user);
 // console.log(user.getuserDetails);
 console.log(this);
 
+// the importance of new:
+
+function  user(username, logInCount, isLoggedIn) {
+    this.username = username;
+    this.logInCount = logInCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`welcome: ${this.username}`);
+    }
+
+    // return this is by default;
+    return this
+}
+
+// the importanvce of new is the everytime you'll get the new value not the overalapped value in the javascript:
+const userOne = new user('dhaval patel', 8, true);
+const userTwo = new user('dhaval', 88, true);
+// console.log(userOne);
+console.log(userOne.constructor);
+console.log(userTwo);
+
+// Example:2:
+
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  const auto = new Car('Honda', 'Accord', 1998);
+  
+  console.log(auto instanceof Car);
+  // Expected output: true
+  
+  console.log(auto instanceof Object);
+  // Expected output: true
+
+
+
 
 
 
