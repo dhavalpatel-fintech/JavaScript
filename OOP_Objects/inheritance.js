c161d29f6e6245e3bb96307ce5fa26e6c618cf2e
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-16 05:32:53 +0000

    inheritance

--- OOP_Objects/inheritance.js
+++ OOP_Objects/inheritance.js
@@ -1,2 +1,30 @@
-// you can use the 'SUPER" in the class function to join the two functions in the java script:
+class User {
+    constructor(username){
+        this.username = username
+    }
 
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
