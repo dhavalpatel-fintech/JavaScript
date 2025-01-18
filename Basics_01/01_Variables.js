1d0af03c13f915aafb1b7cabffb322bef1384023
DhavalPatel_sde <dhavalpatel.sdds@gmail.com>
2025-01-18 20:19:23 +0530

    Update 01_Variables.js

--- Basics_01/01_Variables.js
+++ Basics_01/01_Variables.js
@@ -8,37 +8,37 @@
 
 // // Variable Example:
 
-// // function exmaple(){
-// //     var x = 10;
-// //     var y = 62;
-// //     console.log(x);
-// //     console.log(y)
-// //     return x;
-// // };
-// // exmaple();
-// // console.log(y);
+function exmaple(){
+    var x = 10;
+    var y = 62;
+    console.log(x);
+    console.log(y)
+    return x;
+};
+exmaple();
+console.log(y);
 
 // // being a block function the {let and const} varibale will not console out of the block so defined by the scope {}.
 
 // // let and the const function
 
-// // let x = 10;
-// // if(true){
-// //     console.log(x);
+let x = 10;
+if(true){
+    console.log(x);
 
-// //     let y = 20;
-// //     console.log(y);
-// // }
+    let y = 20;
+    console.log(y);
+}
 
-// const x = 100;
-// if(true){
-//     console.log(x);
+const x = 100;
+if(true){
+    console.log(x);
 
-//     const y = 200;
-//     console.log(y);
-// }
-// console.log(x);
-// // console.log(y);
+    const y = 200;
+    console.log(y);
+}
+console.log(x);
+// console.log(y);
 
 
 // the exmaple with the tables 
